12ddfc1d8f44b29a508a5134195187851015a531
Terry Bu <burea1124@gmail.com>
2014-12-29 21:03:38 -0500

    first commit

---
+++ reverseArrayRecursion.rb
@@ -0,0 +1,21 @@
+array = [1,2,3,4,5]
+
+
+
+#using 3 arguments
+def reverseArray(array, firstIndex, lastIndex)
+	temp = array[firstIndex]
+	array[firstIndex] = array[lastIndex]
+	array[lastIndex] = temp
+	firstIndex = firstIndex + 1
+	lastIndex = lastIndex - 1
+	if (firstIndex < lastIndex)
+		reverseArray(array, firstIndex, lastIndex)
+	else
+		return array
+	end
+end
+#I realized that you can't do this without 3 arguments because you have to do recursion, and then you need those variables to survive
+
+
+print reverseArray(array, 0, array.count-1)
