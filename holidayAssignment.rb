3194b8100e99789edde05e106e272977003999bf
Terry Bu <burea1124@gmail.com>
2014-12-31 11:45:41 -0500

    holiday

---
+++ holidayAssignment.rb
@@ -0,0 +1,9 @@
+=begin
+Write a program that takes in a number and prints it in words. 
+e.g. for 201, it should print “two hundred and one”. Support up to billions.
+
+Convert a decimal input to binary. e.g. for 224, it should print 11100000 - without using any built in functions
+
+Convert a string to a number without using built-in functions e.g for “3.1415” it should convert it to 3.1415 and for “123” it should convert to 123.
+=end
+
