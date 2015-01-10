3c5de281f5a6358d3430f28ec62a59df788affe4
Terry Bu <burea1124@gmail.com>
2015-01-09 21:24:19 -0500

    Got started on merge sort

---
+++ Sorting/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {32,0,21,11,4,3,2,55};
+		mergeSort(array);
+		for (int i : array) {
+			System.out.print(i);
+			System.out.print(" ");
+		}
+	}
+	
+	static void mergeSort(int[] array) {
+		
+	}
+	
+	static void merge(int[] left, int[] right) {
+		
+	}
+	
+}
