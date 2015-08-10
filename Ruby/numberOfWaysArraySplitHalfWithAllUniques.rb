571cc3566cff0838fcfd8f76571dd35f07f26873
Terry Bu <burea1124@gmail.com>
2015-08-10 12:21:27 -0400

    reorganizing objective-c. ALso updated findUniqueHalfCombinations

--- Ruby/numberOfWaysArraySplitHalfWithAllUniques.rb
+++ Ruby/numberOfWaysArraySplitHalfWithAllUniques.rb
@@ -1,4 +1,4 @@
-def returnWays(array)
+def findUniqueHalfCombinations(array)
 	result = 0
 	k = 0
 
@@ -17,8 +17,27 @@ def returnWays(array)
 end
 
 
+testArray = [2,1,1,4,1,1,4,2]
+testArray2 = [1]
+testArray3 = [5,1,5,1,5,1]
 
+p findUniqueHalfCombinations(testArray)
+p findUniqueHalfCombinations(testArray2)
+p findUniqueHalfCombinations(testArray3)
 
-p returnWays([2,1,1,4,1,1,4,2])
-p returnWays([1])
-p returnWays([5,1,5,1,5,1])
+p "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+
+#I like this version better 8/1/2015
+def findUniqueHalfCombinations2(array)
+	count = 0
+	for k in 1...array.length-1
+		if array[0...k].uniq.sort == array[k...array.length].uniq.sort
+			count +=1
+		end
+	end
+	return count
+end
+
+p findUniqueHalfCombinations2(testArray)
+p findUniqueHalfCombinations2(testArray2)
+p findUniqueHalfCombinations2(testArray3)
