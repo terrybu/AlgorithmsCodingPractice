322241eec8ca8554bbe335f3f75f8ab34e00ecb5
Terry Bu <burea1124@gmail.com>
2015-08-22 14:42:38 -0400

    different iterative fibonacci brian jordan udemy

--- Ruby/recursion/fibonacci_iteration.rb
+++ Ruby/recursion/fibonacci_iteration.rb
@@ -9,6 +9,23 @@ def fib(n)
 	p f
 	return f[n]
 end
-
-
 p fib(20)
+
+
+def fibOtherWay(n)
+	#0, 1, 1, 2, 3, 5, 8
+	#a  b 
+	   #a  b  
+	a = 0
+	b = 1
+
+	while (n > 0)
+		oldB = b #old a = 0
+		b = a + b #new b = 0 + 1 
+		a = oldB
+		n = n - 1 
+	end
+
+	return a
+	#only caveat, you calculate an extra b you don't need from this function
+end
