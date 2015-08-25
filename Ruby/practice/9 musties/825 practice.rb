0191b09242160a9afe12bc365a61489968c04d7c
Terry Bu <burea1124@gmail.com>
2015-08-25 12:50:16 -0400

    last minute tips

--- Ruby/practice/9 musties/825 practice.rb
+++ Ruby/practice/9 musties/825 practice.rb
@@ -88,3 +88,82 @@ end
 
 findBullsAndCows(nil, [2])
 findBullsAndCows([2,3,4], [2,0,3])
+
+
+
+
+def fib(n) #find nth term
+	if n == 0
+		return 0
+	elsif n == 1
+		return 1
+	end
+
+	return fib(n-2) + fib(n-1)
+end
+
+p fib(10)
+
+
+def insertionSort(array)
+	#[3,5,0,1,2,6]
+
+	#we are going to start from index 1, the key is that one at i
+	#we are going to go one space to the left at index j
+	#keep sorting the key to its right place, and then move i one to the right
+	#nested loop
+	for i in 1..array.length-1
+		key = array[i]
+		j = i - 1
+		while j >= 0 && array[j] > key
+			array[j+1] = array[j]
+			j = j -1
+		end
+		array[j+1] = key
+	end
+	return array
+end
+
+p insertionSort([3,5,0,1,2,6])
+
+
+def findIfAnagram(str1, str2)
+	#1 definition of an anagram is when rearranged, they equal each other
+	#2 edge cases ... either one of them is empty or null
+	#3 test inputs 
+		# "word" "jaws" --> false
+		# "coat" "taco" --> true
+		# "" "asdf" --> false
+	#4 brainstorm
+		#a) sort both of them, see if they equal to each other
+	
+	#empty or null check, return false
+		#blah
+
+	if str1 == nil || str2 == nil
+		return false
+	end
+	if str1.empty? || str2.empty?
+		return false
+	end
+
+	if insertionSort(str1) == insertionSort(str2)
+		return true
+	end
+	return false
+end
+
+p findIfAnagram("taco", "coat")
+p findIfAnagram("", "asdf")
+p findIfAnagram("dafjpas", "fad")
+
+
+
+
+
+
+
+
+
+
+
